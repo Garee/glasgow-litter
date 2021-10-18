88d287bbd9b22dd8172f06a2cd5c9877f0e293ba
Gary Blackwood <gary@garyblackwood.co.uk>
2021-10-18 19:21:35 +0100

    feat: added --dry-run to fetch_street_images.py
    Former-commit-id: 1fc84bf6b2d16a7da96296bf821a7125f474bd5d

--- scripts/fetch_street_images.py
+++ scripts/fetch_street_images.py
@@ -1,10 +1,18 @@
+# pylint: disable=missing-docstring
+
 """
 This script is an example of how to use the Google Street View Static API
 to download a street view image.
 """
 import os
+import sys
+import json
+import random
+import pathlib
+import argparse
 import requests
 from dotenv import load_dotenv
+from shapely.geometry import shape, Point
 
 load_dotenv()
 
@@ -13,29 +21,24 @@ API_KEY = os.getenv("GOOGLE_MAPS_API_KEY")
 
 IMG_WIDTH = 800
 IMG_HEIGHT = 800
-RADIUS = 50
-FOV = 90
-PITCH = 0
-SOURCE = "outdoor"
-RETURN_ERR_CODE = "true"
 
+GLASGOW_DATA_ZONES = "data/geojson/glasgow-data-zones.geojson.json"
 HTTP_STATUS_OK = 200
 
 
-def main():
-    """
-    Download a street view image at a fixed location.
-    """
-    lat = 47.5763831
-    lon = -122.4211769
+class DownloadImageError(Exception):
+    pass
+
+
+def download_image(lat, lon):
     params = {
         "size": f"{IMG_WIDTH}x{IMG_HEIGHT}",
         "location": f"{lat},{lon}",
-        "fov": FOV,
-        "pitch": PITCH,
-        "radius": RADIUS,
-        "source": SOURCE,
-        "return_error_code": RETURN_ERR_CODE,
+        "fov": 90,
+        "pitch": 0,
+        "radius": 50,
+        "source": "outdoor",
+        "return_error_code": "true",
         "key": API_KEY,
     }
     res = requests.get(API_URL, params)
@@ -44,7 +47,98 @@ def main():
         with open(fname, "wb") as file:
             file.write(res.content)
     else:
-        print(res.text)
+        raise DownloadImageError(res.text)
+
+
+def load_data_zone_geojson():
+    data_zones = []
+    with open(GLASGOW_DATA_ZONES) as file:
+        collection = json.loads(file.read())
+        features = collection["features"]
+        for feature in features:
+            geometry, properties = feature["geometry"], feature["properties"]
+            data_zones.append(
+                {"shape": shape(geometry).buffer(0), "properties": properties}
+            )
+    return data_zones
+
+
+def generate_n_random_points(n_points, polygon):
+    points = []
+    min_x, min_y, max_x, max_y = polygon.bounds
+    while len(points) < n_points:
+        point = Point(random.uniform(min_x, max_x), random.uniform(min_y, max_y))
+        if polygon.contains(point):
+            points.append(point)
+    return points
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(
+        description="Download Google Street View images of Glasgow City."
+    )
+    parser.add_argument(
+        "out_path",
+        type=pathlib.Path,
+        help="directory path in which to store the image files",
+    )
+    parser.add_argument(
+        "--n-images",
+        type=int,
+        default=1,
+        help="number of images to download per data zone (default 1)",
+    )
+    parser.add_argument(
+        "--data-zone",
+        nargs="+",
+        help="data zones for which images should be downloaded",
+    )
+    parser.add_argument(
+        "--dry-run",
+        action="store_true",
+        help="output a JSON object that describes the images that would be downloaded.",
+    )
+    return parser.parse_args()
+
+
+def filter_data_zones(data_zones, by_zones):
+    if not by_zones:
+        return data_zones
+
+    def by_zone(data_zone):
+        name = data_zone["properties"]["DataZone"]
+        return name in by_zones
+
+    return list(filter(by_zone, data_zones))
+
+
+def get_data_zones(filter_by):
+    data_zones = load_data_zone_geojson()
+    return filter_data_zones(data_zones, filter_by)
+
+
+def generate_points(data_zones, n_points=1):
+    for data_zone in data_zones:
+        data_zone["points"] = generate_n_random_points(n_points, data_zone["shape"])
+    return data_zones
+
+
+def dz_to_json(data_zones):
+    rep = {}
+    for data_zone in data_zones:
+        name = data_zone["properties"]["DataZone"]
+        points = data_zone["points"]
+        rep[name] = {"points": [{"lat": p.y, "lon": p.x} for p in points]}
+    return json.dumps(rep)
+
+
+def main():
+    args = parse_args()
+    data_zones = get_data_zones(args.data_zone)
+    data_zones = generate_points(data_zones, args.n_images)
+    if args.dry_run:
+        print(dz_to_json(data_zones))
+    sys.exit(0)
 
 
 if __name__ == "__main__":
