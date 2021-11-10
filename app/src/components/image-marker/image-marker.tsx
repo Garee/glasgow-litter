d6661340b5388ee60a91a376d50c6bec90d8b376
Gary Blackwood <gary@garyblackwood.co.uk>
2021-11-10 21:07:09 +0000

    feat: yolov5 model

--- app/src/components/image-marker/image-marker.tsx
+++ app/src/components/image-marker/image-marker.tsx
@@ -1,8 +1,8 @@
-import { IncomingMessage } from "http";
 import { Icon, LatLngExpression } from "leaflet";
 import React, { FC } from "react";
 import { Marker, Popup } from "react-leaflet";
 import "./image-marker.css";
+import * as detectionMetadata from "../../../../models/yolov5/yolov5/runs/detect/exp/metadata.json";
 
 export interface ImageMarkerProps {
   id: string;
@@ -22,11 +22,24 @@ export const ImageMarker: FC<ImageMarkerProps> = ({
   height,
   path,
 }) => {
+  let iconUrl = "image.png";
+  let className = "image-marker-icon";
+
+  const tokens = path.split("/");
+  const fname = tokens[tokens.length - 1];
+  const images: any = (detectionMetadata as any).images;
+  if (fname in images) {
+    iconUrl = "litter.png";
+    className = "image-marker-icon litter-marker-icon";
+    path = images[fname].path;
+  }
+
   const icon = new Icon({
-    iconUrl: "image.png",
+    iconUrl,
     iconSize: [32, 32],
-    className: "image-marker-icon",
+    className,
   });
+
   const position: LatLngExpression = [lat, lon];
   const maxPopupWidth = undefined; // 100%
   return (
